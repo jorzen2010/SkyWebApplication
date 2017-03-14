3f6515d64f39bd249ea7ded443aa814e25c7e61c
jorzen <jorzen2010@163.com>
2017-03-14 16:36:21 +0800

    表单验证

--- SkyWebApplication/Controllers/SysUserController.cs
+++ SkyWebApplication/Controllers/SysUserController.cs
@@ -9,71 +9,43 @@ using System.Web.Mvc;
 using SkyWebApplication.Models;
 using SkyWebApplication.DAL;
 using PagedList;
-using PagedList.Mvc;
 
 namespace SkyWebApplication.Controllers
 {
-    public class UserController : Controller
+    public class SysUserController : Controller
     {
         private SkyWebContext db = new SkyWebContext();
-        private UnitOfWork unitOfWork = new UnitOfWork();
 
-        // GET: /不带分页
-        //public ActionResult Index()
-        //{
-        //    var sysUsers = unitOfWork.SysUserRepository.Get().ToList();
-        //    return View(sysUsers);
-        //}
-
-
-        // GET: /全部取出数据后分页
-        //public ActionResult Index(int? page)
-        //{
-        //    int pageNumber = page ?? 1;
-        //    int pageSize = 1;
-        //    var sysUsers = unitOfWork.SysUserRepository.Get().ToList();
-        //    return View(sysUsers.ToPagedList(pageNumber, pageSize));
-
-        //}
-        // GET: 按需要取数据分页
+        // GET: /获取分页数据/
         public ActionResult Index(int? page)
         {
             int pageIndex = page ?? 1;
-            int pageSize = 1;
+            int pageSize = 2;
             int totalCount = 0;
             var sysUsers = GetsysUsers(pageIndex, pageSize, ref totalCount);
             var sysUsersAsIPageList = new StaticPagedList<SysUser>(sysUsers, pageIndex, pageSize, totalCount);
             return View(sysUsersAsIPageList);
 
         }
-        // GET: Ajax方式获取数据，分页方式带解决
-        public ActionResult List(int? page)
-        {
-            return View();
-        
-        }
-
-        public ActionResult AjaxPage(int? page)
-        {
-            int pageIndex = page ?? 1;
-            int pageSize = 1;
-            int totalCount = 0;
-            var sysUsers = GetsysUsers(pageIndex, pageSize, ref totalCount);
-            var sysUsersAsIPageList = new StaticPagedList<SysUser>(sysUsers, pageIndex, pageSize, totalCount);
-            return Json(new { sysUsers = sysUsersAsIPageList, pager = sysUsersAsIPageList.GetMetaData() }, JsonRequestBehavior.AllowGet);
-        }
 
         public List<SysUser> GetsysUsers(int pageIndex, int pageSize, ref int totalCount)
         {
             var sysUsers = (from p in db.SysUsers
-                           orderby p.ID descending
-                           select p).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+                            orderby p.ID descending
+                            select p).Skip((pageIndex - 1) * pageSize).Take(pageSize);
             totalCount = db.SysUsers.Count();
             return sysUsers.ToList();
 
         }
 
-        // GET: /User/Details/5
+        //原来的首页，准备删除
+        //// GET: /SysUser/
+        //public ActionResult Index()
+        //{
+        //    return View(db.SysUsers.ToList());
+        //}
+
+        // GET: /SysUser/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -88,13 +60,13 @@ namespace SkyWebApplication.Controllers
             return View(sysuser);
         }
 
-        // GET: /User/Create
+        // GET: /SysUser/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: /User/Create
+        // POST: /SysUser/Create
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
@@ -111,7 +83,7 @@ namespace SkyWebApplication.Controllers
             return View(sysuser);
         }
 
-        // GET: /User/Edit/5
+        // GET: /SysUser/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -126,7 +98,7 @@ namespace SkyWebApplication.Controllers
             return View(sysuser);
         }
 
-        // POST: /User/Edit/5
+        // POST: /SysUser/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
@@ -142,7 +114,7 @@ namespace SkyWebApplication.Controllers
             return View(sysuser);
         }
 
-        // GET: /User/Delete/5
+        // GET: /SysUser/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -157,7 +129,7 @@ namespace SkyWebApplication.Controllers
             return View(sysuser);
         }
 
-        // POST: /User/Delete/5
+        // POST: /SysUser/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
---
+++ SkyWebApplication/ViewModels/SysUserViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkyWebApplication.ViewModels
+{
+    public class SysUserViewModel
+    {
+
+        public  int ID { get; set; }
+        [Required]
+        [Display(Name = "用户名")]
+        public  string UserName { get; set; }
+        [Required]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+    }
+}
